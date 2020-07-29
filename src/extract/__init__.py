51895264e65117d4e754d58367836377c27418d8
Juan Caio <jcpsantos@github.com>
2020-07-28 22:36:11 -0300

    melhorias na funçao get_quote

--- src/extract/__init__.py
+++ src/extract/__init__.py
@@ -2,7 +2,9 @@ import urllib.request
 from bs4 import BeautifulSoup
 import datetime
 import pandas as pd
+from retrying import retry
 
+@retry(stop_max_attempt_number=7, wait_fixed = 2000)
 def get_quote(path, inst, cod, value):
     valor_de_compra = value
     data = datetime.date.today()
@@ -23,9 +25,9 @@ def get_quote(path, inst, cod, value):
     for hr in horario_:
         horario.append(hr.get_text())
 
-    valor_atual = abertura[0]
+    valor_atual = abertura[9]
     
     #Criando o dataframe
-    df = pd.DataFrame({'instituicao':[inst],"codigo": [cod], "data":[data], "horario": [horario[0]],'valor_compra':[valor_de_compra], 'abertura':[float(abertura[3])], 'valor_atual': [float(valor_atual)]})
+    df = pd.DataFrame({'instituicao':[inst],"codigo": [cod], "data":[data], "horario": [horario[0]],'valor_compra':[valor_de_compra], 'abertura':[float(abertura[12])], 'valor_atual': [float(valor_atual)]})
     
     return df
